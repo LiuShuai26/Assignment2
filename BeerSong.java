ba9584df5fccc3a4cf1f8ad507a23bac907c82cb
Shuai Liu <shuailiu@oakland.edu>
2018-01-28 12:29:04 -0500

    finnish the function ninety_Nine_Bottles_of_Beer() but output is not we expected

--- BeerSong.java
+++ BeerSong.java
@@ -1,6 +1,11 @@
 public class BeerSong{
-	public static void Ninety_Nine_Bottles_of_Beer(){
-		System.out.println("99 bottles of beer on the wall, 99 bottles of beer");
+	public static void ninety_Nine_Bottles_of_Beer(){
+		int bottles = 99;
+		while (bottles > 0){
+			System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer");
+			bottles--;
+			System.out.println("take one down, pass it around, " + bottles + " bottles of beer on the wall");
+		}
 	}
 	public static void main(String []args){
 
