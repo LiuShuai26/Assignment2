29b160af28f366ca40a560273bcb76274b008e69
Shuai Liu <shuailiu@oakland.edu>
2018-01-28 12:35:38 -0500

    modified function ninety_Nine_Bottles_of_Beer(), We can get the correct output now

--- BeerSong.java
+++ BeerSong.java
@@ -1,13 +1,16 @@
-public class BeerSong{
+public class BeerSong {
 	public static void ninety_Nine_Bottles_of_Beer(){
 		int bottles = 99;
 		while (bottles > 0){
 			System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer");
 			bottles--;
-			System.out.println("take one down, pass it around, " + bottles + " bottles of beer on the wall");
+			if (bottles > 0)	//print diffrent sentence when no more bottles on the wall
+				System.out.println("take one down, pass it around, " + bottles + " bottles of beer on the wall");
+			else
+				System.out.println("take one down, pass it around, no more bottles of beer on the wall");
 		}
 	}
 	public static void main(String []args){
-
+		ninety_Nine_Bottles_of_Beer();
 	}
 }
