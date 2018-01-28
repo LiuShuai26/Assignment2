9731b9a663e39e1bde7ae32eb9939c23219ca2fe
Shuai Liu <shuailiu@oakland.edu>
2018-01-28 12:14:40 -0500

    Add BeerSong.java with empty mian() and ninety_Nine_Bottles_of_Beer() functions

---
+++ BeerSong.java
@@ -0,0 +1,8 @@
+public class BeerSong{
+	public static void Ninety_Nine_Bottles_of_Beer(){
+		System.out.println("99 bottles of beer on the wall, 99 bottles of beer");
+	}
+	public static void main(String []args){
+
+	}
+}
